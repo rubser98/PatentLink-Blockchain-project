7f8f6b40d2be64a4573ccdf2de5d38cd664d018c
rubser98 <rubser17@gmail.com>
2023-12-26 21:27:19 +0100

    openzeppelin dependencies

--- contracts/PatentToken.sol
+++ contracts/PatentToken.sol
@@ -1,16 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract PatentToken is ERC20, Ownable {
-    constructor(address initialOwner)
-        ERC20("PatentToken", "PTNT")
-        Ownable(initialOwner)
-    {}
+contract PatentToken is ERC20,ERC20Capped, ERC20Burnable{
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    address public owner;
+    uint public blockReward;
+
+    /// @notice  throws an error if the msg.sender is not the owner.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this.");
+        _;
     }
+
+    constructor(uint cap, uint reward)
+        ERC20("PatentToken", "PTNT") ERC20Capped(cap * (10 ** decimals())){
+            owner = msg.sender;
+            _mint(owner, 70000000 * (10 ** decimals()));
+            blockReward = reward * (10 ** decimals());
+        }
+
+    function setBlockReward(uint newReward) public onlyOwner {
+        blockReward = newReward * (10 ** decimals());
+    }
+
+    function _mintMinerReward() internal{
+        _mint(block.coinbase, blockReward);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint value) internal virtual override{
+        if(from != address(0) && to != block.coinbase && block.coinbase != address(0)){
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from,to,value);   
+    }
+
+    function destroy() public onlyOwner(){
+        selfdestruct(owner);
+    }
+
+
 }
